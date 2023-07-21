d4ef90d6b3b8ec3889bf1ff9aecfd9397b1b7bc3
Prih0d <taptanga3@gmail.com>
2023-07-21 22:08:08 +0300

    first

---
+++ main.js
@@ -0,0 +1,66 @@
+// const yearsUser = prompt('сколько тебе лет?');
+// const doYouSmoke = prompt('куришь?');
+
+// if(yearsUser >= 18 && doYouSmoke == 'да'){
+//     alert('Ну и зря');
+// }
+
+// if(yearsUser >= 18 && doYouSmoke == 'нет'){
+//     alert('Молодец, так и надо');
+// }
+
+// if(yearsUser < 18 && doYouSmoke == 'да'){
+//     alert('Маме расскажу');
+// }
+
+// if(yearsUser < 18 && doYouSmoke == 'нет'){
+//     alert('так держать');
+// }
+
+// if(!yearsUser || !doYouSmoke){
+//     alert('а ты что-то забыл написать');
+// }
+
+
+
+// const firstNumber = prompt('введите первое число');
+// const secondNumber = prompt('Введите второе число');
+
+// if(firstNumber > secondNumber){
+//     alert(`это число ${firstNumber} больше этого ${secondNumber}`);
+// }
+    
+// if(firstNumber < secondNumber){
+//     alert(`это число '${secondNumber}' больше этого '${firstNumber}'`);
+// } 
+
+// if(firstNumber == secondNumber){
+//     alert(`это число '${secondNumber}' равно этому '${firstNumber}'`);
+// }
+
+
+// const kilometr = prompt('Укажите километраж');
+// const feet = prompt('Укажите футаж))');
+
+// let feetToKilometr = feet * 0.305;
+
+// switch (key) {
+//     case value:
+        
+//         break;
+
+//     default:
+//         break;
+// }
+
+// if(kilometr > feetToKilometr){
+//     alert(`${kilometr} километров больше ${feet} футов`)
+// }
+
+// if(kilometr < feetToKilometr){
+//     alert(`${feet} футов больше ${kilometr} километров`)
+// }
+
+// if(kilometr == feetToKilometr){
+//     alert(`'${kilometr}' километров равно '${feet}' футам`)
+// }
