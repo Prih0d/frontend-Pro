d4ce6e0d7d08b5f5625579a241bfa29d66011d24
Prih0d <taptanga3@gmail.com>
2023-07-21 23:57:06 +0300

    final

--- main.js
+++ main.js
@@ -1,66 +1,79 @@
-// const yearsUser = prompt('сколько тебе лет?');
-// const doYouSmoke = prompt('куришь?');
+let numberKvest = prompt('Какое задание хотите выбрать?\n1) Проверка на возрат и курение \n2) Больше меньше число\n3) Больше меньше километры к футам')
 
-// if(yearsUser >= 18 && doYouSmoke == 'да'){
-//     alert('Ну и зря');
-// }
+switch (numberKvest) {
+    case '1':
+        firstKvest()
+        break;
+    case '2':
+        secondKvest()
+        break;
+    case '3':
+        thirdKvest()
+        break;
 
-// if(yearsUser >= 18 && doYouSmoke == 'нет'){
-//     alert('Молодец, так и надо');
-// }
+    default:
+        console.log('Введите цифру 1, 2 или 3');
+        break;
+}
 
-// if(yearsUser < 18 && doYouSmoke == 'да'){
-//     alert('Маме расскажу');
-// }
+function firstKvest() {
+    const yearsUser = prompt('сколько тебе лет?');
+    const doYouSmoke = prompt('куришь?');
 
-// if(yearsUser < 18 && doYouSmoke == 'нет'){
-//     alert('так держать');
-// }
+    if(yearsUser >= 18 && doYouSmoke == 'да'){
+        alert('Ну и зря');
+    }
 
-// if(!yearsUser || !doYouSmoke){
-//     alert('а ты что-то забыл написать');
-// }
+    if(yearsUser >= 18 && doYouSmoke == 'нет'){
+        alert('Молодец, так и надо');
+    }
 
+    if(yearsUser < 18 && doYouSmoke == 'да'){
+        alert('Маме расскажу');
+    }
 
+    if(yearsUser < 18 && doYouSmoke == 'нет'){
+        alert('так держать');
+    }
 
-// const firstNumber = prompt('введите первое число');
-// const secondNumber = prompt('Введите второе число');
+    if(!yearsUser || !doYouSmoke){
+        alert('а ты что-то забыл написать');
+    }
+}
 
-// if(firstNumber > secondNumber){
-//     alert(`это число ${firstNumber} больше этого ${secondNumber}`);
-// }
-    
-// if(firstNumber < secondNumber){
-//     alert(`это число '${secondNumber}' больше этого '${firstNumber}'`);
-// } 
+function secondKvest() {
+    const firstNumber = prompt('введите первое число');
+    const secondNumber = prompt('Введите второе число');
 
-// if(firstNumber == secondNumber){
-//     alert(`это число '${secondNumber}' равно этому '${firstNumber}'`);
-// }
-
-
-// const kilometr = prompt('Укажите километраж');
-// const feet = prompt('Укажите футаж))');
-
-// let feetToKilometr = feet * 0.305;
-
-// switch (key) {
-//     case value:
+    if(firstNumber > secondNumber){
+        alert(`это число ${firstNumber} больше этого ${secondNumber}`);
+    }
         
-//         break;
+    if(firstNumber < secondNumber){
+        alert(`это число '${secondNumber}' больше этого '${firstNumber}'`);
+    } 
 
-//     default:
-//         break;
-// }
+    if(firstNumber == secondNumber){
+        alert(`это число '${secondNumber}' равно этому '${firstNumber}'`);
+    }
+}
 
-// if(kilometr > feetToKilometr){
-//     alert(`${kilometr} километров больше ${feet} футов`)
-// }
+function thirdKvest() {
+    const kilometr = prompt('Укажите километраж');
+    const feet = prompt('Укажите футаж))');
 
-// if(kilometr < feetToKilometr){
-//     alert(`${feet} футов больше ${kilometr} километров`)
-// }
+    let feetToKilometr = feet * 0.305;
 
-// if(kilometr == feetToKilometr){
-//     alert(`'${kilometr}' километров равно '${feet}' футам`)
-// }
+    if(kilometr > feetToKilometr){
+        alert(`${kilometr} километров больше ${feet} футов`)
+    }
+
+    if(kilometr < feetToKilometr){
+        alert(`${feet} футов больше ${kilometr} километров`)
+    }
+
+    if(kilometr == feetToKilometr){
+        alert(`'${kilometr}' километров равно '${feet}' футам`)
+    }
+
+}
