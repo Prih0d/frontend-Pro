bde3af71623dec04f472ec6a83a8c8f9685a3aa0
Prih0d <taptanga3@gmail.com>
2023-07-22 16:52:56 +0300

    final dz3

--- main.js
+++ main.js
@@ -1,18 +1,20 @@
 let soldierColorArmor = prompt('Цвет доспеха');
 let soldierImageShield = prompt('Картинка на щите');
 
-// function isLannisterSoldier(colorArmor, imageShield) {
-//     colorArmor === 'red' || imageShield === 'lion' ? console.log('Its Lanister') : console.log('Its not Lanister')
-
-// }
-
 function isLannisterSoldier(colorArmor, imageShield) {
-    if(colorArmor === 'red' || imageShield === 'lion'){
-        console.log('Its Lanister');
-    } else{
-        console.log('Its not Lanister')
-    }
+     return colorArmor === 'red' || imageShield === 'lion' ? true : false
 }
 
+// function isLannisterSoldier(colorArmor, imageShield) {
+//     if(colorArmor === 'red' || imageShield === 'lion'){
+//         return true
+//     } else{
+//         return false
+//     }
+// }
 
-isLannisterSoldier(soldierColorArmor, soldierImageShield)
+function outputIsLannister(func) {
+    func ? console.log('Its Lannister') : console.log('Its not Lannister');
+}
+
+outputIsLannister(isLannisterSoldier(soldierColorArmor, soldierImageShield))
